7288ab57f429b9eaa994bf18684f033e1723ca70
lemon-zf <zhang_91026@163.com>
2016-03-03 20:52:36 +0800

    初始化

---
+++ person-sort/src/main/java/leaderus/study/person/sort/App.java
@@ -0,0 +1,13 @@
+package leaderus.study.person.sort;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
